fbdd9e39cd89a05643741976871d02da3f7816a2
anano303 <124179773+anano303@users.noreply.github.com>
2023-12-12 00:34:50 +0400

    2 davaleba

---
+++ script.js
@@ -0,0 +1,22 @@
+// const div = document.getElementById("none");
+// const btn = document.getElementById("btnn");
+
+// btn.addEventListener("click", (e) => {
+//   e.target.style.color = "red";
+//   div.style.display = "none";
+// });
+// btn.addEventListener("mouseout", (e) => {
+//   e.target.style.color = "green";
+//   div.style.display = "block";
+// });
+
+// const divEl = document.createElement("div");
+// const h2El = document.createElement("h1");
+// const aEl = document.createElement("a");
+// const textEl = document.createTextNode("Gandalf");
+// const textEl2 = document.createTextNode("Go to profile");
+
+// h2El.append(textEl);
+// aEl.append(textEl2);
+// divEl.append(h2El, aEl);
+// document.body.append(divEl);
